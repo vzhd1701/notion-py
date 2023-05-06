723c3859e34168dc9679781b87f312f9afcdd397
vzhd1701 <vzhd1701@gmail.com>
2023-05-06 12:02:23 +0500

    fix: change TLS signature to avoid 403 from cloudflare

--- notion/utils_ssl.py
+++ notion/utils_ssl.py
@@ -38,7 +38,9 @@ def create_custom_urllib3_context():
     https://stackoverflow.com/questions/64967706/python-requests-https-code-403-without-but-code-200-when-using-burpsuite
     """
 
-    context = create_urllib3_context()
+    CIPHERS = ('DEFAULT:@SECLEVEL=2')
+
+    context = create_urllib3_context(ciphers=CIPHERS)
     set_minimum_tls_version_1_2(context)
     context.options &= ~ssl.OP_NO_TICKET
 
